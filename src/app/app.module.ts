48d3342b1a129d57e35b65c3e310d57814cddb8a
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2017-09-25 16:50:04 +0200

    Styling improvements, unused component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,12 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { PasswordResetComponent } from './components/password-reset/password-reset.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { StgcButtonComponent } from './components/stgc-button/stgc-button.component';
-import { StgcInputComponent } from './components/stgc-input/stgc-input.component';
 import { PasswordSetupComponent } from './components/password-setup/password-setup.component';
 import { StgcPasswordComponent } from './components/stgc-password/stgc-password.component';
 import { PasswordChangeSuccessComponent } from './components/password-change-success/password-change-success.component'
 
 const appRoutes: Routes = [
-  { path: '', component: LoginPageComponent },
-  { path: 'reset', component: PasswordResetComponent },
+  { path: '', component: PasswordResetComponent },
   { path: 'setup', component: PasswordSetupComponent },
   { path: 'success', component: PasswordChangeSuccessComponent },
   { path: '**', component: PageNotFoundComponent },
@@ -27,7 +25,6 @@ const appRoutes: Routes = [
     PasswordResetComponent,
     LoginPageComponent,
     StgcButtonComponent,
-    StgcInputComponent,
     PasswordSetupComponent,
     StgcPasswordComponent,
     PasswordChangeSuccessComponent
